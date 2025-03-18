6b095ec49223b343baa756afb0d403b449ccf3a5
Germán Rojas <g.rojasc@uniandes.edu.co>
2025-03-17 20:55:26 -0500

    Enhance UsersModule with user properties and enums for state and subscription status

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { UserState, SubscriptionStatus } from '../../common/enums';
 
 @Module({})
-export class UsersModule {}
+export class UsersModule {
+    id: number;
+    username: string;
+    password: string;
+    mail_address: string;
+    profile_picture: string;
+    user_rating: number;
+    whishlists: number[];
+    status: UserState;
+    subscription: SubscriptionStatus;
+}
