5b039e8a82498cc98a1e4eabf191645a7a94dafe
Germán Rojas <g.rojasc@uniandes.edu.co>
2025-03-17 21:03:15 -0500

    Add User model with properties for user details and status

---
+++ src/modules/users/user.model.ts
@@ -0,0 +1,14 @@
+import { UserState, SubscriptionStatus } from '../../common/enums';
+
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    mail_address: string;
+    profile_Picture: string;
+    user_rating: number;
+    wishlists: number[];
+    status: UserState;
+    subscription: SubscriptionStatus;
+  }
+  
