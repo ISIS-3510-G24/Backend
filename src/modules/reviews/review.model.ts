793ba9952887413d5ed6dd9555f9a716b373fc3e
Germán Rojas <g.rojasc@uniandes.edu.co>
2025-03-18 01:04:39 -0500

    Add user review functionality and update user rating in UsersService

--- src/modules/reviews/review.model.ts
+++ src/modules/reviews/review.model.ts
@@ -1,7 +1,8 @@
 export class Review {
     id: string;
     reviewer_id: string;
-    product_id: string;
+    product_id?: string;
+    reviewed_user_id?: string;
     review_rating: number;
     review_message: string;
     review_image?: string;
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { ReviewsService } from '../reviews/reviews.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, ReviewsService]
 })
 export class UsersModule {}
