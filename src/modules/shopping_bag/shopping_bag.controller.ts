2321634f8f894a2ce5b307f44c1ac39f8466f899
NEXAF7 <juanmanuelrt2001@gmail.com>
2025-03-18 00:46:58 -0500

    Update shopping_bag.controller.ts

--- src/modules/shopping_bag/shopping_bag.controller.ts
+++ src/modules/shopping_bag/shopping_bag.controller.ts
@@ -1,47 +1,58 @@
 import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
 import { ShoppingBagService } from './shopping_bag.service';
 import { ShoppingBag } from './shopping_bag.model';
+
 @Controller('shopping-bag')
 export class ShoppingBagController {
-    constructor(private readonly productRequestService: ProductRequestService) {}
+    constructor(private readonly shoppingBagService: ShoppingBagService) {}
     
         @Post()
-        async createProductRequest(@Body() productRequest: ProductRequest): Promise<void> {
-            await this.productRequestService.createProductRequest(productRequest);
+        async createShoppingBag(@Body() shoppingBag: ShoppingBag): Promise<void> {
+            await this.shoppingBagService.createShoppingBag(shoppingBag);
         }
     
         @Get(':id')
-        async getProductRequestById(@Param('id') id: string): Promise<ProductRequest> {
-            return this.productRequestService.getProductRequestById(id);
+        async getShoppingBagById(@Param('id') id: string): Promise<ShoppingBag> {
+            return this.shoppingBagService.getShoppingBagById(id);
         }
     
         @Put(':id')
-        async updateProductRequest(@Param('id') id: string, @Body() data: Partial<ProductRequest>): Promise<void> {
-            await this.productRequestService.updateProductRequest(id, data);
+        async updateShoppingBag(@Param('id') id: string, @Body() data: Partial<ShoppingBag>): Promise<void> {
+            await this.shoppingBagService.updateShoppingBag(id, data);
         }
     
         @Delete(':id')
-        async deleteProductRequest(@Param('id') id: string): Promise<void> {
-            await this.productRequestService.deleteProductRequest(id);
+        async deleteShoppingBag(@Param('id') id: string): Promise<void> {
+            await this.shoppingBagService.deleteShoppingBag(id);
         }
     
         @Put(':id/product/:productId')
-        async linkRequestToProduct(@Param('id') requestId: string, @Param('productId') productId: string): Promise<void> {
-            await this.productRequestService.linkRequestToProduct(requestId, productId);
+        async linkShoppingToProduct(@Param('id') shoppingId: string, @Param('productId') productId: string): Promise<void> {
+            await this.shoppingBagService.linkShoppingToProduct(shoppingId, productId);
         }
     
         @Delete(':id/product')
-        async unlinkRequestFromProduct(@Param('id') requestId: string): Promise<void> {
-            await this.productRequestService.unlinkRequestFromProduct(requestId);
+        async unlinkShoppingFromProduct(@Param('id') shoppingId: string): Promise<void> {
+            await this.shoppingBagService.unlinkShoppingFromProduct(shoppingId);
         }
     
         @Put(':id/user/:userId')
-        async addRequestToUser(@Param('id') requestId: string, @Param('userId') userId: string): Promise<void> {
-            await this.productRequestService.addRequestToUser(userId, requestId);
+        async addShoppingToUser(@Param('id') shoppingId: string, @Param('userId') userId: string): Promise<void> {
+            await this.shoppingBagService.addShoppingToUser(userId, shoppingId);
         }
     
         @Delete(':id/user/:userId')
-        async removeRequestFromUser(@Param('id') requestId: string, @Param('userId') userId: string): Promise<void> {
-            await this.productRequestService.removeRequestFromUser(userId, requestId);
+        async removeShoppingFromUser(@Param('id') shoppingId: string, @Param('userId') userId: string): Promise<void> {
+            await this.shoppingBagService.removeShoppingFromUser(userId, shoppingId);
+        }
+
+        @Put(':id/order/:orderId')
+        async addShoppingToOrder(@Param('id') shoppingId: string, @Param('orderId') orderId: string): Promise<void> {
+            await this.shoppingBagService.addShoppingToOrder(orderId, shoppingId);
+        }
+    
+        @Delete(':id/order/:orderId')
+        async removeShoppingFromOrder(@Param('id') shoppingId: string, @Param('orderId') orderId: string): Promise<void> {
+            await this.shoppingBagService.removeShoppingFromOrder(orderId, shoppingId);
         }
 }
