ff3cde017af1da80bb33538026325b9e33c2621c
Germán Rojas <g.rojasc@uniandes.edu.co>
2025-03-17 20:53:35 -0500

    Add enums for UserState, SubscriptionStatus, and OrderStatus

---
+++ src/common/enums.ts
@@ -0,0 +1,18 @@
+export enum UserState {
+    NEW = 'new',
+    ACTIVE = 'active',
+    BLOCKED = 'blocked',
+    BANNED = 'banned',
+}
+
+export enum SubscriptionStatus {
+    FREE = 'free',
+    WEEKLY = 'weekly',
+    MONTHLY = 'monthly',
+    YEARLY = 'yearly',
+}
+
+export enum OrderStatus {
+    UNFINISHED = 'Unfinished',
+    PAYMENT_COMPLETE = 'Payment_complete',
+}
