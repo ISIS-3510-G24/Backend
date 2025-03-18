2060607b0fb27a62ce5a5960c7a57a18afa400db
Germán Rojas <g.rojasc@uniandes.edu.co>
2025-03-17 22:13:45 -0500

    Add Product model with properties for product details

---
+++ src/modules/products/product.model.ts
@@ -0,0 +1,11 @@
+export class Product {
+    product_id: number;
+    prod_quantity: number;
+    prod_name: string;
+    prod_image: string;
+    prod_price: number;
+    prod_description: string;
+    prod_characteristics: string[];
+    prod_availability: string;
+}
+  
